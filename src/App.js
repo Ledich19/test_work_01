bead74ef4aa02797ef1feff72c9c2ffb3aae5072
Aleksandr <ledich19@.com>
2022-07-15 09:34:27 +0300

    morning revision :)

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
 import axios from 'axios'
-import {useHref, useLocation, useMatch, useNavigate, useParams, useSearchParams} from 'react-router-dom'
+import { useLocation,  useNavigate } from 'react-router-dom'
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
-// api за 
-// /search/parse_link
-// /search/styles
 
 function App() {
   
@@ -21,21 +20,15 @@ function App() {
   const [services, setServices] = useState(null);
   const [brends, setBrends] = useState(null);
   const [style, setStyle] = useState(null);
-  const navigate = useNavigate()
 
   const urlAddres = useLocation()
-
-
-
-
+  const navigate = useNavigate()
 
   useEffect(() => {
     axios.get('https://autobooking.com/api/test/v1/search/terms').then(response => {
     const services = response.data
     setServices(services.data)
-
   })
-
     axios.get('https://autobooking.com/api/test/v1/search/brands_terms').then(response => {
     const services = response.data
     setBrends(services.data)
@@ -43,25 +36,20 @@ function App() {
     axios.get('https://autobooking.com/api/test/v1/search/styles').then(response => {
     const services = response.data
     setStyle(services.data)
-
   })
-},[]);
+  },[]);
+  
   useEffect(() => {
-    const services = servicesItem.slug  ? servicesItem.slug : ''
-    const brends = brendsItem.slug ? brendsItem.slug : ''
-    const style = styleItem.slug ? styleItem.slug : ''
+    const services = servicesItem.slug  ? servicesItem.slug : urlAddres.pathname.match('(\/s-).*?\/')[0].slice(3,-1)
+    const brends = brendsItem.slug ? brendsItem.slug : urlAddres.pathname.match('(\/b-).*?\/')[0].slice(3,-1)
+    const style = styleItem.slug ? styleItem.slug : urlAddres.pathname.match('(\/st-).*?\/')[0].slice(4,-1)
     navigate(`/s-${services}/b-${brends}/st-${style}/`)
-},[servicesItem, brendsItem, styleItem, navigate]);
-  useEffect(() => {
-    console.log('match',urlAddres.pathname);
+  },[servicesItem, brendsItem, styleItem, navigate]);
 
-    const re1 = new RegExp('(\/s-).*?\/');
-    const re2 = new RegExp('(\/b-).*?\/');
-    const re3 = new RegExp('(\/st-).*?\/');
-    const str1 = urlAddres.pathname.match(re1)
-    const str2 = urlAddres.pathname.match(re2)
-    const str3 = urlAddres.pathname.match(re3)
-    console.log('params',str1[0].slice(3,-1),str2[0].slice(3,-1) ,str3[0].slice(4,-1))
+  useEffect(() => {
+    const str1 = urlAddres.pathname.match('(\/s-).*?\/')[0].slice(3,-1)
+    const str2 = urlAddres.pathname.match('(\/b-).*?\/')[0].slice(3,-1)
+    const str3 = urlAddres.pathname.match('(\/st-).*?\/')[0].slice(4,-1)
     axios
       .get(`https://autobooking.com/api/test/v1/search/parse_link?service_slug=${str1}&brand_slug=${str2}&style_slug=${str3}`)
       .then(response => {})
@@ -80,11 +68,21 @@ function App() {
       </ListSubheader>
     }
   >
-
-      <ListItem name='Послуги' value={servicesItem.label} items={services} setItem={ setServicesItem } />
-      <ListItem name='Бренди' value={brendsItem.label} items={brends} setItem={  setBrendsItem } /> 
-      <ListItem name='Стилі' value={styleItem.label} items={style} setItem={  setStyleItem } />
-   
+      <ListItem 
+        name='Послуги' 
+        value={servicesItem.label} 
+        items={services} 
+        setItem={ setServicesItem } />
+      <ListItem 
+        name='Бренди' 
+        value={brendsItem.label} 
+        items={brends} 
+        setItem={  setBrendsItem } /> 
+      <ListItem 
+        name='Стилі' 
+        value={styleItem.label} 
+        items={style} 
+        setItem={  setStyleItem } />
       </List>
   );
 }
@@ -94,6 +92,7 @@ function App() {
 const ListItem = ({ items, value , setItem, name}) => {
   const [open, setOpen] = useState(false);
   
+
   if (!items) {
     return null
   }
@@ -111,14 +110,14 @@ const ListItem = ({ items, value , setItem, name}) => {
      <>
       <ListSubheader onClick={handleOpen} component="div" id="nested-list-subheader">
         { name }
+        {open ? <ExpandLess className='arov'  onClick={handleOpen}/> : <ExpandMore className='arov' onClick={handleOpen}/>}
       </ListSubheader>
 
-    <ListItemButton onClick={handleOpen} >
+    <ListItemButton  >
       <ListItemIcon>
       </ListItemIcon>
       <ListItemText primary={value} />
     </ListItemButton>
-
     <Collapse in={open} timeout="auto" unmountOnExit>
       <List component="div" disablePadding>
         {items.map(s => {
